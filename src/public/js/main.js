671e97e4090b1c2288a13a0bd082fc9d9317e20d
Agustin <rodriguezjoseagustin31@gmail.com>
2022-06-14 14:59:08 -0400

    Build header and carousel

---
+++ src/public/js/main.js
@@ -0,0 +1,12 @@
+// Slider settings
+$(document).ready(function () {
+	$('.slider').slick({
+		Infinite: true,
+		autoplay: true,
+		slidesToShow: 10,
+		slidesToScroll: 1,
+		accessibility: false,
+		arrows: false,
+		autoplaySpeed: 2000,
+	});
+});
